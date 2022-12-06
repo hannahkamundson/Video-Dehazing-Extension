f3d5be1c2caa0fbf4ba7ac9ad9a78dc1cf8b78e3
hannahkamundson <hannahkamundson@gmail.com>
2022-12-06 17:09:13 -0600

    WIP

--- code/model/__init__.py
+++ code/model/__init__.py
@@ -19,47 +19,8 @@ class Model(nn.Module):
 
         module = import_module('model.' + args.model.lower())
         self.model = module.make_model(args).to(self.device)
-#         if not args.cpu and args.n_GPUs > 1:
-#             self.model = nn.DataParallel(self.model, range(args.n_GPUs))
-        
-        # DDP setting
-        if "WORLD_SIZE" in os.environ:
-            args.world_size = int(os.environ["WORLD_SIZE"])
-        args.distributed = args.world_size > 1
-        ngpus_per_node = torch.cuda.device_count()
-
-        if args.distributed:
-            if args.local_rank != -1: # for torch.distributed.launch
-                args.rank = args.local_rank
-                args.gpu = args.local_rank
-            elif 'SLURM_PROCID' in os.environ: # for slurm scheduler
-                args.rank = int(os.environ['SLURM_PROCID'])
-                args.gpu = args.rank % torch.cuda.device_count()
-            dist.init_process_group(backend=args.dist_backend, init_method=args.dist_url,
-                                    world_size=args.world_size, rank=args.rank)
-
-        # suppress printing if not on master gpu
-        if args.rank!=0:
-            def print_pass(*args):
-                pass
-            builtins.print = print_pass
-
-        ### model ###
-        if args.distributed:
-            # For multiprocessing distributed, DistributedDataParallel constructor
-            # should always set the single device scope, otherwise,
-            # DistributedDataParallel will use all available devices.
-            if args.gpu is not None:
-                torch.cuda.set_device(args.gpu)
-                self.model.cuda(args.gpu)
-                self.model = torch.nn.parallel.DistributedDataParallel(model, device_ids=[args.gpu])
-#                 model_without_ddp = self.model.module
-            else:
-#                 self.model.cuda()
-                self.model = torch.nn.parallel.DistributedDataParallel(self.model)
-#                 model_without_ddp = self.model.module
-        else:
-            raise NotImplementedError("Only DistributedDataParallel is supported.")
+        if not args.cpu and args.n_GPUs > 1:
+            self.model = nn.DataParallel(self.model, range(args.n_GPUs))
 
         self.load(
             ckp.dir,
