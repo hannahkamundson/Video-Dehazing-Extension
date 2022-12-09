74b72ae1ae2df54e462a1241f6abf21b73c375d6
hannahkamundson <hannahkamundson@gmail.com>
2022-12-09 01:31:43 -0600

    WIP

--- code/utils/print.py
+++ code/utils/print.py
@@ -3,4 +3,5 @@ import os
 # I am sure there is a better way to do this but I don't want to spend too much time learning Python's logging system
 def print_pretty(*args, **kwargs):
     global_rank = os.environ['SLURM_PROCID']
-    print(f'r{global_rank}', *args, **kwargs)
+    node_id=os.environ['SLURM_NODEID']
+    print(f'{node_id}r{global_rank}', *args, **kwargs)
