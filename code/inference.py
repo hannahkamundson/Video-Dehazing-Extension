6f2edbbd7c2dfb1f91d90eba514a3856d6dae2dd
hannahkamundson <hannahkamundson@gmail.com>
2022-12-11 17:02:36 -0600

    Updating path

--- code/inference.py
+++ code/inference.py
@@ -272,7 +272,7 @@ if __name__ == '__main__':
     elif args.quick_test == 'REVIDE_64':
         args.data_path = '/scratch/08310/rs821505/Video-Dehazing-Extension/dataset/REVIDE_REDUCED/Test/hazy'
         args.gt_path = '/scratch/08310/rs821505/Video-Dehazing-Extension/dataset/REVIDE_REDUCED/Test/gt'
-        args.model_path = '/scratch/08310/rs821505/Video-Dehazing-Extension/experiment/REVIDE_REDUCED/20221210_19.51/Dehaze/model/model_319_han_test.pt'
+        args.model_path = '/scratch/08310/rs821505/Video-Dehazing-Extension/experiment/REVIDE_REDUCED/20221210_19.51/Dehaze/model/model_best.pt'
         args.infer_flag = 'SOTS_outdoor'
         args.save_image = True
         args.infer_flag = datetime.datetime.now().strftime('%Y%m%d_%H.%M')
@@ -280,7 +280,7 @@ if __name__ == '__main__':
     elif args.quick_test == 'REVIDE_128':
         args.data_path = '/scratch/08310/rs821505/Video-Dehazing-Extension/dataset/REVIDE_REDUCED128/Test/hazy'
         args.gt_path = '/scratch/08310/rs821505/Video-Dehazing-Extension/dataset/REVIDE_REDUCED128/Test/gt'
-        args.model_path = '/scratch/08310/rs821505/Video-Dehazing-Extension/experiment/REVIDE_REDUCED/20221210_21.58/Dehaze/model/model_322_han_test.pt'
+        args.model_path = '/scratch/08310/rs821505/Video-Dehazing-Extension/experiment/REVIDE_REDUCED/20221210_21.58/Dehaze/model/model_best.pt'
         args.infer_flag = datetime.datetime.now().strftime('%Y%m%d_%H.%M')
         args.save_image = True
         args.result_path = '../infer_results/REVIDE_128'
